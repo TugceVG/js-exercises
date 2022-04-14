61a2e9ac955eea7827a8254e875994c9c0db203c
TugceVG <97672418+TugceVG@users.noreply.github.com>
2022-04-14 19:46:48 +0200

    Removed some unused code and renamed array.

--- calculator/script.js
+++ calculator/script.js
@@ -1,21 +1,20 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-    const array = document.getElementsByClassName('cell');
-    const array2 = ['c', '/', '*', 'del', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '.', '(', '0', ')', '='];
+    const buttons = document.getElementsByClassName('cell');
     const specialChars = ['c', 'del', '='];
-    const c = document.getElementById('c');
+    const clearAllBtn = document.getElementById('clear-all-btn');
     const del = document.getElementById('del');
     const display = document.querySelector('.display');
 
-    c.addEventListener('click', clearAll)
+    clearAllBtn.addEventListener('click', clearAll)
     del.addEventListener('click', deteleItem)
 
-    function selection(array) {
-        for (let i = 0; i < array.length; i++) {
-            const key = array[i].innerHTML;
+    function selection(buttons) {
+        for (let i = 0; i < buttons.length; i++) {
+            const key = buttons[i].innerHTML;
             const isKeyInList = specialChars.includes(key.toLowerCase());
             if (!isKeyInList) {
-                array[i].addEventListener('click', function (e) {
+                buttons[i].addEventListener('click', function (e) {
                     display.innerHTML = display.innerHTML + key
                 })
             }
@@ -23,16 +22,15 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     function deteleItem() {
 
-        const valueToArray = (display.innerHTML.split(""));
-        valueToArray.pop();
-        // valueToArray[valueToArray.length - 1] = '';
-        let arrayToString = (valueToArray.toString()).replace(/\,/g, "");
+        const displayToArray = display.innerHTML.split("");
+        displayToArray.pop();
+        let arrayToString = (displayToArray.toString()).replace(/\,/g, "");
         console.log(display.innerHTML = arrayToString);
     }
     function clearAll() {
-        display.textContent = '0';
+        display.textContent = '';
     }
-    selection(array)
+    selection(buttons)
 })
 
 
