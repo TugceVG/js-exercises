27fb540e11e9c13ed3d23b4d757158fa0ce8aa9b
Tugce Vatansever GUNDUZ <tugcevatansevergunduz@Tugces-Air.fritz.box>
2022-10-19 23:10:41 +0200

    create quiz constructor

---
+++ quiz-app/script.js
@@ -0,0 +1,34 @@
+function Question(questionText, options, correctAnswer) {
+    this.questionText = questionText;
+    this.options = options;
+    this.correctAnswer = correctAnswer;
+}
+
+Question.prototype.answerCheck = function (answer) {
+    return this.correctAnswer === answer
+}
+
+let questions = [
+    new Question("Hangisi Javascript paket yonetim uygulamasidir?", { a: "Node.js", b: "Typescript", c: "Npm", d: "Npx" }, "c"),
+    new Question("Hangisi .net paket yonetim uygulamasidir?", { a: "Node.js", b: "Nuget", c: "Npm", d: "Npx" }, "b"),
+]
+
+function Quiz(questions) {
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestion = function () {
+    return this.questions[this.questionIndex];
+}
+
+const quiz = new Quiz(questions);
+
+document.querySelector(".btn-start").addEventListener("click", function () {
+    if (quiz.questions.length != quiz.questionIndex) {
+        console.log(quiz.getQuestion());
+        quiz.questionIndex += 1;
+    } else {
+        console.log("End of Quiz");
+    }
+})
