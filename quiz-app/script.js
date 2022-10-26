664f4a690a6f079d98e420257d08884e5db8c507
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2022-10-26 15:40:34 +0200

    add functionality for the correct answer

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -28,6 +28,7 @@ document.querySelector(".btn_start").addEventListener("click", function () {
     document.querySelector(".quiz_box").classList.add("active")
     let question = quiz.getQuestion();
     showQuestion(question);
+    document.querySelector(".next_btn").classList.remove("show");
 })
 
 document.querySelector(".next_btn").addEventListener("click", function () {
@@ -35,14 +36,21 @@ document.querySelector(".next_btn").addEventListener("click", function () {
         quiz.questionIndex += 1;
         let question = quiz.getQuestion();
         showQuestion(question);
+        document.querySelector(".next_btn").classList.remove("show");
     } else {
         console.log("End of Quiz");
     }
 });
 
+const option_list = document.querySelector(".option_list");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
+
 function showQuestion(questionItem) {
     let question = `<span>${questionItem.questionText}</span>`;
     let options = '';
+
+    console.log(questionItem.options)
 
     for (let answer in questionItem.options) {
         options +=
@@ -52,7 +60,33 @@ function showQuestion(questionItem) {
         </div>
         `
     }
+
     document.querySelector(".question_text").innerHTML = question;
-    document.querySelector(".option_list").innerHTML = options;
+    option_list.innerHTML = options;
+
+    const option = option_list.querySelectorAll(".option");
+
+    for (let opt of option) {
+        opt.setAttribute("onclick", "optionSelected(this)")
+    }
+}
+
+function optionSelected(option) {
+    let answer = option.querySelector("span b").textContent;
+    let question = quiz.getQuestion();
+
+    if (question.answerCheck(answer)) {
+        option.classList.add("correct");
+        option.insertAdjacentHTML("beforeend", correctIcon);
+    } else {
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend", incorrectIcon);
+    }
+
+    for (let i = 0; i < option_list.children.length; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+
+    document.querySelector(".next_btn").classList.add("show");
 
 }
