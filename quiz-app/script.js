c9f9efc7a36ad6ea999e42c51af75b27a3ee33e4
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2022-10-26 13:15:52 +0200

    show question dynamically

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -25,11 +25,34 @@ Quiz.prototype.getQuestion = function () {
 const quiz = new Quiz(questions);
 
 document.querySelector(".btn_start").addEventListener("click", function () {
-    if (quiz.questions.length != quiz.questionIndex) {
-        document.querySelector(".quiz_box").classList.add("active")
-        console.log(quiz.getQuestion());
+    document.querySelector(".quiz_box").classList.add("active")
+    let question = quiz.getQuestion();
+    showQuestion(question);
+})
+
+document.querySelector(".next_btn").addEventListener("click", function () {
+    if (quiz.questions.length != quiz.questionIndex + 1) {
         quiz.questionIndex += 1;
+        let question = quiz.getQuestion();
+        showQuestion(question);
     } else {
         console.log("End of Quiz");
     }
-})
+});
+
+function showQuestion(questionItem) {
+    let question = `<span>${questionItem.questionText}</span>`;
+    let options = '';
+
+    for (let answer in questionItem.options) {
+        options +=
+            `
+        <div class="option">
+        <span><b>${answer}</b>:${questionItem.options[answer]} </span>
+        </div>
+        `
+    }
+    document.querySelector(".question_text").innerHTML = question;
+    document.querySelector(".option_list").innerHTML = options;
+
+}
