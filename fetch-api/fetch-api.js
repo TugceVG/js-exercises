3e53958a07b2e453df206ee10c786398c5564ed8
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-03-05 23:02:38 +0100

    Remove .DS_Store from everywhere

--- fetch-api/fetch-api.js
+++ fetch-api/fetch-api.js
@@ -22,7 +22,6 @@ const getPosts = async () => {
     //Fetching data by Axios Fetch
     try {
         const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-        // the comment from remote !
         writeToScreen(res.data);
     } catch (e) {
         console.log(e)
