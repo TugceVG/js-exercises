32b55a62c4a4d0a8c60b5c482a09fc94b85fe45f
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-10-11 23:37:55 +0200

    chore(rental-car-app): update styles with theme

--- rental-car-app/script.js
+++ rental-car-app/script.js
@@ -1,6 +1,26 @@
 const toggleButton = document.getElementById('toggle-button');
-const navbar = document.getElementById('nav-links');
+const navbar = document.getElementById('navbar');
+const bulbButton = document.getElementById('bulbButton');
+const bulb = document.getElementById('bulb');
 
 toggleButton.addEventListener('click', () => {
     navbar.classList.toggle('active');
+});
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme) {
+    document.documentElement.setAttribute('data-theme', storedTheme)
+}
+
+bulbButton.addEventListener('click', () => {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "dark";
+
+    if (currentTheme === "dark") {
+        targetTheme = "light";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+    bulb.classList.toggle('dark');
 });
