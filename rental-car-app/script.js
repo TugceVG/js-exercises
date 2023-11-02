6d7913a98db30a7519dc77b97ba39b617437bb0c
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-11-02 21:44:23 +0100

    chore(rental-car-app): change theme icon and add opengraph

--- rental-car-app/script.js
+++ rental-car-app/script.js
@@ -1,26 +1,39 @@
 const toggleButton = document.getElementById('toggle-button');
 const navbar = document.getElementById('navbar');
-const bulbButton = document.getElementById('bulbButton');
-const bulb = document.getElementById('bulb');
+
+const themeButton = document.getElementById('themeButton');
+const sun = document.getElementById('sun');
+const moon = document.getElementById('moon');
 
 toggleButton.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
 
 var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
 if (storedTheme) {
-    document.documentElement.setAttribute('data-theme', storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme);
 }
 
-bulbButton.addEventListener('click', () => {
+if(storedTheme==="light") {
+    sun.classList.add('disabled');
+}else{
+    moon.classList.add('disabled');
+}
+
+themeButton.addEventListener('click', () => {
     var currentTheme = document.documentElement.getAttribute("data-theme");
     var targetTheme = "dark";
 
     if (currentTheme === "dark") {
         targetTheme = "light";
+        moon.classList.remove('disabled');
+        sun.classList.add('disabled')
+    }else{
+        moon.classList.add('disabled');
+        sun.classList.remove('disabled');
     }
 
     document.documentElement.setAttribute('data-theme', targetTheme)
     localStorage.setItem('theme', targetTheme);
-    bulb.classList.toggle('dark');
 });
